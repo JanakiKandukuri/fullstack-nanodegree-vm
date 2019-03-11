098c4466c33327d9d0de8290709784123bc20b74
janaki kandukuri <janaki0424@gmail.com>
2019-03-11 00:26:03 -0500

    changed routes names and fixed google sign in issue ****

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -32,10 +32,9 @@ session = DBSession()
 metadata = MetaData()
 session.commit()
 
-
+# google sign in route
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
-    print 'Executing gconnect'
     with open('client_secrets.json', 'r') as file:
         CLIENT_ID = json.load(file)['web']['client_id']
     # Verify Anti Forgery State Token received from client
@@ -99,75 +98,16 @@ def gconnect():
     data = answer.json()
     login_session['username'] = data['name']
     login_session['email'] = data['email']
-    # Check if the user already exists in the database
+    login_session['picture'] = data['picture']
     user_id = getUserID(login_session['email'])
     if not user_id:
         user_id = createUser(login_session)
     login_session['user_id'] = user_id
-    # Send Response back to the client
-    output = ''
-    output += '<h1>Welcome '
-    output += login_session['username']
-    output += '</h1>'
-    return output
-    # if(request.args.get('state') != login_session['state']):
-    #     response = make_response(json.dumps('Invalid state token'), 401)
-    #     response.headers['Content-Type'] = 'application/json'
-    #     return response
-    # code = request.data
-    # try:
-    #     oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
-    #     oauth_flow.redirect_uri = 'postmessage'
-    #     print(code)
-    #     credentials = oauth_flow.step2_exchange(code)
-    #     print(credentials.access_token)
-    # except FlowExchangeError:
-    #     response = make_response(
-    #                 json.dumps('Failed upgrading authorization code'), 401)
-    #     response.headers['Content-Type'] = 'application/json'
-    #     return response
-    # access_token = credentials.access_token
-    # url = 'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token  # noqa
-    # h = httplib2.Http()
-    # result = json.loads(h.request(url, 'GET')[1])
-    # # if error in retriving token abort and return error
-    # if (result.get('error') is not None):
-    #     response = make_response(json.dumps(result.get('error')), 500)
-    #     response.headers['Content-Type'] = 'application/json'
-    # # verify if we have right access token
-    # gplus_id = credentials.id_token['sub']
-    # if (result['user_id'] != gplus_id):
-    #     response = make_response(
-    #                 json.dumps('Token user_id doesnt match given user'), 401)
-    #     response.headers['Content-Type'] = 'application/json'
-    #     return response
-    # # check to see if users already logged in into system
-    # stored_credentials = login_session.get('credentials')
-    # stored_gplus_id = login_session.get('gplus_id')
-    # if (stored_credentials is not None and gplus_id == stored_gplus_id):
-    #     response = make_response(
-    #                 json.dumps(' current user is already connected'), 200)
-    #     response.headers['Content-Type'] = 'application/json'
-    # # valid access token in session for later use
-    # login_session['access_token'] = credentials.access_token
-    # login_session['gplus_id'] = gplus_id
-
-    # # user info
-    # userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
-    # params = {'access_token': credentials.access_token, 'alt': 'json'}
-    # answer = requests.get(userinfo_url, params=params)
-    # data = answer.json()
-    # login_session['username'] = data['name']
-    # login_session['email'] = data['email']
-    # login_session['picture'] = data['picture']
-    # user_id = getUserID(login_session['email'])
-    # if not user_id:
-    #     user_id = createUser(login_session)
-    # login_session['user_id'] = user_id
-    # user_info = getUserInfo(user_id)
-    # return jsonify(UserInfo=[i.serialize for i in user_info])
+    user_info = getUserInfo(user_id)
+    return jsonify(UserInfo=[i.serialize for i in user_info])
 
 
+# google logout route
 @app.route('/gdisconnect')
 def gdisconnect():
     # only disconnect a connected user
@@ -180,6 +120,7 @@ def gdisconnect():
     url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
     h = httplib2.Http()
     result = (h.request(url, 'GET')[0])
+    print(result)
     if (result['status'] == '200'):
         del login_session['username']
         del login_session['access_token']
@@ -196,101 +137,87 @@ def gdisconnect():
         return response
 
 
+# ****** DIFFERENT ROUTES FOR USER NAVIGATION **********
+# Initial Route / Categories
 @app.route('/')
 @app.route('/categories')
 def categories():
+    users = session.query(UserInfo).all()
+    for u in users:
+        print(u.id)
     if('username' in login_session):
         user_id = getUserID(login_session['email'])
-        return redirect(url_for('userCatalog',user_id=user_id))
+        return redirect(url_for('userCatalog', user_id=user_id))
     else:
         catalog = session.query(Catalog).all()
+        items = session.query(CatalogItem).all()
         for c in catalog:
             print(c.name)
+            print(c.user_id)
         state = ''.join(
                     random.choice(string.ascii_uppercase +
                                   string.digits)for x in range(32))
         login_session['state'] = state
-        return render_template('categories.html', catalog=catalog,STATE=state)
+        return render_template('categories.html', catalog=catalog, STATE=state)
 
 
-@app.route('/categories/<int:cat_id>')
-def categoryItem(cat_id):
-    if(cat_id):
-        cat_items = session.query(
+# METHOD: GET
+# Router for category Items
+@app.route('/categories/<int:category_id>')
+def categoryItem(category_id):
+    if(category_id):
+        catalog_items = session.query(
                     CatalogItem).filter_by(
-                        catalog_id=cat_id).all()
+                        catalog_id=category_id).all()
         catalog = session.query(Catalog).all()
-        return render_template('main.html', catalog=catalog,items=cat_items)
+        return jsonify(CatalogItem=[i.serialize for i in catalog_items])
 
 
-@app.route('/categories/login/<int:user_id>')
+# METHOD: GET
+# Router for user login
+@app.route('/categories/user/<int:user_id>')
 def userCatalog(user_id):
     user_catalog = session.query(Catalog).filter_by(user_id=user_id).all()
     all_catalog = session.query(Catalog).all()
-    catalog = {'userCatalog': user_catalog, 'all': all_catalog}
-    return render_template('user_login_catalog.html', catalog=catalog) 
+    catalog = {'userCatalog': user_catalog, 'user_id': user_id, 'all': all_catalog}
+    return render_template('user_login_catalog.html', catalog=catalog)
 
 
-# @app.route('/categories/login/<int:user_id>/new', methods=['GET', 'POST'])
-# def newCatalogMenu(user_id):
-#     if(user_id):
-#         if (request.method == 'POST'):
-#             if (request.form['sport_name'] != ''):
-#                 user_id = getUserID(login_session['email'])
-#                 newCat = Catalog(name=request.form['sport_name'], user_id=user_id)
-#                 session.add(newCat)
-#                 session.flush()
-#                 session.refresh(newCat)
-#                 flash('New Sport added to Catalog')
-#                 message = {"name": request.form['sport_name'], "id": newCat.id}
-#                 return render_template('new_catalog.html', message=message)
-#         else:
-#             catalog = session.query(Catalog).all()
-#             return render_template('new_catalog.html', catalog=catalog)        
-#     else:
-#         return redirect('/')
+@app.route('/categories/user/<int:user_id>/create', methods=['GET', 'POST'])
+def createCatalog(user_id):
+    if(user_id):
+        if (request.method == 'POST'):
+            if (request.form['sport_name'] != ''):
+                user_id = getUserID(login_session['email'])
+                newCat = Catalog(name=request.form['sport_name'],
+                                 user_id=user_id)
+                session.add(newCat)
+                session.flush()
+                session.refresh(newCat)
+                flash('New Sport added to Catalog')
+                message = {"name": request.form['sport_name'], "id": newCat.id}
+                return render_template('new_catalog.html', message=message)
+        else:
+            create = {'user_id': user_id}
+            return render_template('new_catalog.html', catalog_new=create)
+    else:
+        return redirect('/')
 
 
-# @app.route('/categories/login/<int:user_id>/<int:catalog_id>/newItem', methods=['POST'])
-# def newCatalogMenuItem(user_id,catalog_id):
-#     catalogItems = session.query(
-#                     CatalogItem).filter_by(
-#                         catalog_id=catalog_id).all()
-#     if (request.method == 'POST'):
-#         items = json.loads(request.data)
-#         if(items):
-#             user_id = getUserID(login_session['email'])
-#             for item in items:
-#                 newItems = CatalogItem(name=item[0], description=item[1],
-#                                        user_id=user_id, catalog_id=catalog_id)
-#                 session.add(newItems)
-#                 session.commit()
-#                 return redirect(url_for('userCatalog', user_id=user_id))
-
-
-# @app.route('/categories/login/<int:user_id>/<int:catalog_id>/delete', methods=['POST'])
-# def deleteCatalog(user_id,catalog_id):
-#     deleteCat = session.query(Catalog).filter_by(id=catalog_id).one()
-#     if (request.method == 'POST'):
-#         session.delete(deleteCat)
-#         session.commit()
-#         flash('Item deleted')
-#         return 'success'
-
-
-# @app.route('/categories/login/<int:user_id>/<int:catalog_id>/edit', methods=['GET','POST'])
-# def editCatalog(user_id,catalog_id):
-#     catalog = session.query(Catalog).filter_by(id=catalog_id).one()
-#     catalogItems = session.query(
-#                     CatalogItem).filter_by(
-#                         catalog_id=catalog_id).all()
-#     edit = {'catalog': catalog, 'catalogItems': catalogItems}
-#     if (request.method == 'GET'):
-#         return render_template('user_catalog_edit.html', catalog_edit=edit) 
+@app.route('/categories/user/<int:user_id>/category/<int:catalog_id>',
+           methods=['GET', 'POST'])
+def editCatalog(user_id, catalog_id):
+    catalog = session.query(Catalog).filter_by(id=catalog_id).one()
+    catalogItems = session.query(
+                    CatalogItem).filter_by(
+                        catalog_id=catalog_id).all()
+    edit = {'catalog': catalog, 'catalogItems': catalogItems,
+            'user_id': user_id}
+    if (request.method == 'GET'):
+        return render_template('user_catalog_edit.html', catalog_edit=edit)
 
 
 # helper functions for users
-
 def createUser(login_session):
     newUser = UserInfo(name=login_session['username'],
                        email=login_session['email'],
--- vagrant/catalog/static/scripts/main_script.js
+++ vagrant/catalog/static/scripts/main_script.js
@@ -1,119 +1,5 @@
-$(document).ready(function () {
-
-    /** clicking on settings gear/login a tag in container */
-    $('#setting_gear').on('click', function () {
-        $('.dropdown-menu').toggle();
-    })
-
-    /** clicking on catalog a tag in container */
-    $('.catalog').click(function () {
-        var id = $(this).data("id");
-        console.log(id);
-        $("#items_types").html("")
-        $.ajax({
-            url: "/categories/" + id + "/item",
-            type: "get",
-            success: function (response) {
-                console.log(response)
-                var str = "<ul>";
-                for (var i = 0; i < response.CatalogItem.length; i++) {
-                    str += "<li><a href='javascript:void(0)' data-item_id=" + response.CatalogItem[i].id + " data-id=" + id +
-                        " class='item'>" + response.CatalogItem[i].name +
-                        "</a>" + "<div>" + response.CatalogItem[i].description + "</div>"
-                }
-                str += "</ul>";
-                $("#cat_items").html(str);
-            },
-            error: function (xhr) {
-                console.log(xhr)
-                //Do Something to handle error
-            }
-        });
-    });
-
-    /** clicking on sub item a tag in container */
-    $(document).on('click', 'a.item', function () {
-        var id = $(this).data("id");
-        var item_id = $(this).data("item_id");
-        console.log(id);
-        $.ajax({
-            url: "/categories/" + id + "/" + item_id + "/type",
-            type: "get",
-            success: function (response) {
-                console.log(response)
-                var str = "<ul>";
-                for (var i = 0; i < response.CatalogItemType.length; i++) {
-                    str += "<li><div >" + response.CatalogItemType[i].name +
-                        "</div>" + "<div > " + response.CatalogItemType[i].description + "</div>"
-                }
-                str += "</ul>";
-                $("#items_types").html(str);
-            },
-            error: function (xhr) {
-                console.log(xhr)
-            }
-        });
-    });
-
-});
-
-
-
-
-// function signInCallback(authResult) {
-
-    
-//     if (authResult['code']) {
-//         var STATE = $('#signinButton').data('state');
-//         console.log(STATE)
-//         $('.dropdown-menu').toggle();
-//         $.ajax({
-//             type: "post",
-//             url: '/gconnect?state=' + STATE,
-//             processData: false,
-//             data: authResult['code'],
-//             contentType: 'application/octet-stream; charset=utf-8',
-//             success: function (result) {
-//                 if (result) {
-//                     console.log(result)
-//                     login_session = result['UserInfo'][0];
-//                     document.cookie = "login_session=" + JSON.stringify(login_session);
-//                     userInfoShowHide(true);
-//                     window.location.href = "/user_catalog";
-//                     // $('#user_name').html(login_session['name'])
-//                 } else if (authResult['error']) {
-//                     console.log("error")
-//                 } else {
-//                     $('#reuslt').html('failed .....')
-//                 }
-//             }
-//         })
-//     }
-// }
-
-/** clicking on settings gear logout button */
-$('#signOut').click(function () {
-    $.ajax({
-        type: "get",
-        url: '/gdisconnect',
-        processData: false,
-        contentType: 'application/octet-stream; charset=utf-8',
-        success: function (result) {
-            if (result) {
-                login_session = null;
-                document.cookie = "login_session=;expires=Thu, 01 Jan 1970 00:00:01 GMT;"
-                userInfoShowHide(false)
-            } else if (authResult['error']) {
-                console.log("error")
-            } else {
-                $('#reuslt').html('failed .....')
-            }
-        }
-    })
-})
-
-/** show/ hide when user is logged in */
-function userInfoShowHide(show) {
+ /** HELPER FUNCTIONS */
+ function userInfoShowHide(show) {
     if (show) {
         $('.user_info').show();
         $('#signOutButton').show();
@@ -136,13 +22,250 @@ function userInfoShowHide(show) {
 }
 
 
-// function onSignIn(googleUser) {
-//     console.log("here")
-//     var profile = googleUser.getBasicProfile();
-//     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-//     console.log('Name: ' + profile.getName());
-//     console.log('Image URL: ' + profile.getImageUrl());
-//     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-//   }
+$(document).ready(function () {
+    /** **DOCUMENT READY ACTIONS ** */
+    $('#myList').addClass('active');
+    if(document.cookie.includes('login_session')) {
+        var cookieLogin = document.cookie.split(";")[2];
+        var login_session = (JSON.parse(cookieLogin.split("=")[1]))
+        $('.user_info').show();
+        $('#signOutButton').show();
+        $('#signinButton').hide();
+        $('.user_name').html(login_session['name']);
+        $('.create_new').show();
+        if (login_session['picture']) {
+            $('.avatar').show();
+            $('#avatar_img').attr('src', login_session['picture']);
+        }
+    } else {
+        $('.user_info').hide();
+        $('#signOutButton').hide();
+        $('.avatar').hide();
+        $('#signinButton').show();
+        $('.create_new').hide();
+        $('#settings_menu').hide();
+    }
+});
+
+
+   
+
+/** **METHOD: SIGNIN BUTTON CLICK
+   * **DESC: IN CATALOG MAIN PAGE, CLICK SIGNIN BTN TO LOGIN AS USER */
+$('#signinButton').click(function () {
+    function signInCallback(authResult) {
+        var STATE = $('#signinButton').data('state')
+        if (authResult['code']) {
+            $('.dropdown-menu').toggle();
+            $.ajax({
+                type: 'POST',
+                url: '/gconnect?state=' + STATE,
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest'
+                },
+                contentType: 'application/octet-stream; charset=utf-8',
+                success: function (result) {
+                    login_session = result['UserInfo'][0];
+                    document.cookie = "login_session=" + JSON.stringify(login_session);
+                    window.location.href = '/categories/user/'+login_session['id']
+                    userInfoShowHide(true);
+                    
+                },
+                processData: false,
+                data: authResult['code']
+            });
+        } else {
+            // handle error
+            console.log('There was an error: ' + authResult['error']);
+            $('#result').html('Failed to make a server-side call. Check your configuration and console.');
+        }
+    }
+    auth2.grantOfflineAccess().then(signInCallback);
+});
+
+/** **METHOD: SIGNOUT BUTTON CLICK
+   * **DESC: IN USERS PAGES, CLICK SIGNOUT BTN TO NAVIGATE TO MAIN PAGE */
+$('#signOut').click(function () {
+    $.ajax({
+        type: "get",
+        url: '/gdisconnect',
+        processData: false,
+        contentType: 'application/octet-stream; charset=utf-8',
+        success: function (result) {
+            if (result) {
+                login_session = null;
+                document.cookie = "login_session=;expires=Thu, 01 Jan 1970 00:00:01 GMT;"
+                userInfoShowHide(false)
+            } else if (authResult['error']) {
+                console.log("error")
+            } else {
+                $('#reuslt').html('failed .....')
+            }
+        }
+    })
+})
+
+
+/** **METHOD: MYLIST BUTTON CLICK
+   * **DESC: IN USERS MAIN PAGE, CLICK MYLIST BTN TO VIEW USERS LIST OF CATALOG */
+$('#myList').click(function () {
+    $('#myList').addClass('active');
+    $('#allItems').removeClass('active');
+    $('#all_list_table').hide();
+    $('#user_list_table').show();
+    $('.create_new_btn').show();
+})
+
+/** **METHOD: TOOGLE SETTINGS DROPDOWN TO LOGIN */
+$('#setting_gear').on('click', function () {
+    $('.dropdown-menu').toggle();
+})
+
+/** **METHOD: ALLITEMS BUTTON CLICK
+ * **DESC: IN USERS MAIN PAGE, CLICK ALLITEMS BTN TO VIEW ALL CATALOG */
+$('#allItems').click(function () {
+    $('#allItems').addClass('active');
+    $('#myList').removeClass('active');
+    $('#all_list_table').show();
+    $('#user_list_table').hide();
+    $('.create_new_btn').hide();
+});
+
+
+/** **METHOD: DELETE CATALOG BUTTON CLICK
+ * **DESC: IN USERS MAIN PAGE, CLICK DELTE OR 'X' BTN TO DELETE AN ENTRY FROM LIST OF CATALOG */
+$('.delete_catalog').click(function () {
+    var id = $(this).data("id");
+    $.ajax({
+        url: "/user_catalog/" + id + "/delete",
+        type: "post",
+        success: function (response) {
+            window.location.href = "user_catalog"
+
+        },
+        error: function (xhr) {
+            console.log(xhr)
+        }
+    });
+})
+
+/** **METHOD: EDIT BUTTON CLICK
+ * **DESC: IN USERS MAIN PAGE, CLICK EDIT BTN TO VIEW EDIT AN ENTRY FROM CATALOG */
+$('.edit_catalog').click(function () {
+    var id = $(this).data("id");
+    $.ajax({
+        url: "/user_catalog/" + id + "/edit",
+        type: "get",
+        success: function (response) {
+            console.log(response)
+        },
+        error: function (xhr) {
+            console.log(xhr)
+        }
+    });
+})
+
+/** clicking on catalog a tag in container */
+$('.catalog').click(function () {
+    var id = $(this).data("id");
+    console.log(id);
+    $("#items_types").html("")
+    $.ajax({
+        url: "/categories/" + id,
+        type: "get",
+        success: function (response) {
+            console.log(response)
+            var str = "<ul>";
+            for (var i = 0; i < response.CatalogItem.length; i++) {
+                str += "<li><a href='javascript:void(0)' data-item_id=" + response.CatalogItem[i].id + " data-id=" + id +
+                    " class='item'>" + response.CatalogItem[i].name +
+                    "</a>" + "<div>" + response.CatalogItem[i].description + "</div>"
+            }
+            str += "</ul>";
+            $("#cat_items").html(str);
+        },
+        error: function (xhr) {
+            console.log(xhr)
+            //Do Something to handle error
+        }
+    });
+});
+
+/** clicking on sub item a tag in container */
+$(document).on('click', 'a.item', function () {
+    var id = $(this).data("id");
+    var item_id = $(this).data("item_id");
+    console.log(id);
+    $.ajax({
+        url: "/categories/" + id + "/" + item_id + "/type",
+        type: "get",
+        success: function (response) {
+            console.log(response)
+            var str = "<ul>";
+            for (var i = 0; i < response.CatalogItemType.length; i++) {
+                str += "<li><div >" + response.CatalogItemType[i].name +
+                    "</div>" + "<div > " + response.CatalogItemType[i].description + "</div>"
+            }
+            str += "</ul>";
+            $("#items_types").html(str);
+        },
+        error: function (xhr) {
+            console.log(xhr)
+        }
+    });
+});
+
+
+/** ********************** ON CREATE DISABLE TEXT FIELD AND ENABLE BUTTONS **************** */
+$('.newCat_create_btn').click(function () {
+    $('sport_name').prop('disabled', true)
+})
+
+
+/** ********************** ON ADD BUTTON CLICK ENABLE SAVEALL AND FORM FOR EQUIPMENTS **************** */
+$('.newCat_Add_btn').click(function () {
+    $('.saveall_items').show()
+    newChild = '<div class="sport_item"><label class="input_label">Equipment Name:</label><input type="text" name="equipment_name" class="equipment_name ml-10" /><label class="input_label ml-17">Description:</label><textarea name="description" rows="2" cols="20" class="description ml-10" value="{{c.description}}"></textarea><a href="javascript:void(0)" class="removeCatalogItem">X<br></a></div>'
+    $('.items_form').append(newChild)
+})
+
+
+/** ********************** ON REMOVE CLICK REMOVE SPECIFIED ROW **************** */
+$(document).on("click", ".removeCatalogItem", function () {
+    $(this).parent().remove()
+});
+
+/** ********************** ON SAVE ALL SAVE ALL EQUIPMENTS AND NAVIGATE TO USER HOME PAGE **************** */
+$('.newCat_saveall_items').click(function () {
+    var id = $('.sport_name').data("id");
+    var equipment_names = [];
+    var descriptions = [];
+    var items = [];
+    $('.equipment_name').each(function () {
+        equipment_names.push(($(this).val()));
+    });
+    $('.description').each(function () {
+        descriptions.push(($(this).val()));
+    });
+    for (var i = 0; i < equipment_names.length; i++) {
+        items.push([equipment_names[i], descriptions[i]])
+    }
+    console.log(items)
+    $.ajax({
+        type: "post",
+        url: '/user_catalog/' + id + '/newItem',
+        processData: false,
+        data: JSON.stringify(items),
+        contentType: 'application/octet-stream; charset=utf-8',
+        success: function (result) {
+            if (result) {
+                window.location.href = result
+            } else {
+                $('#reuslt').html('failed .....')
+            }
+        }
+    })
+})
+
 
 
